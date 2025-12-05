3f714a6760fb39e0ad55d6544273665fa298a552
emanuelvpc <emanuelvpc06@gmail.com>
2025-12-05 08:48:16 -0400

    Correcciones de UI: Sidebar, Iconos y Layout principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,6 +44,8 @@
   <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
   <link id="pagestyle" href="../assets/css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
+  @livewireStyles
 </head>
 
 <body class="g-sidenav-show  bg-gray-100 {{ (\Request::is('rtl') ? 'rtl' : (Request::is('virtual-reality') ? 'virtual-reality' : '')) }} ">
@@ -85,6 +87,7 @@
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="../assets/js/soft-ui-dashboard.min.js?v=1.0.3"></script>
+  @livewireScripts
 </body>
 
 </html>
